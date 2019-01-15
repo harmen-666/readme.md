20e341957289a1748d608cb2f94d7fdb19e4f500
karremanx <46673106+karremanx@users.noreply.github.com>
2019-01-15 11:45:11 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,58 +1,6 @@
-function herhaal (teller)
-{for(var i=0; i<teller; i++)}
-{document.writeln(i+" keer");
+function herhaal (teller){
+  for(var i=0; i<teller; i++)}
+  document.writeln(i + " keer");
 }
-}
-document.wrteln("Waters of chaos
-Have invaded all space
-The flood on earth again
-I have to find the whales
-That once did guide us
-To dry lands of life
-I wont despair
-Ill break this dark all around
 
-Under the heavy sea
-Ill search the flight of whales
-
-Beneath the seas I searched
-And had a different view
-Of us on earth:
-The sinking ship of men
-But its beyond the stars
-I found the place
-Where they were
-And they finally came to light
-
-Over the winds
-They dwell in light
-Like the arrow in the sky
-I found myself on higher grounds
-From up here
-For I see them in flight
-
-Now I can see the whales
-Looming out of the dark
-Like arrows in the sky
-I cant believe my eyes
-But its 
-
-Huge tunnel ends in light
-Like banks of clouds they gather
-See massive shape of flesh
-Swimming giants in the clear
-The mightiest comes to me
-Im on the wing, wide open
-They teach me how to fly
-Slowly moving in the air
-They dance
-
-So much told with no words at all
-Powerful presence for only speech
-Breath
-
-Over the winds
-They dwell in light
-Fly
-")
-document.readline();
+document.writeln("dit is beter");
